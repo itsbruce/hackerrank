e7abb8e9b7031d13df5770f1a99bdd63f2d26006
Bruce Richardson <it.is.bruce@gmail.com>
2016-06-28 16:10:21 +0100

    Somehow the code from another solution had been pasted over Sums of Powers (hs)

--- func/recur/sumsOfPowers.hs
+++ func/recur/sumsOfPowers.hs
@@ -1,20 +1,13 @@
-object Solution {
-    
-    def fibonacci(x:Int):Int = {
-        def loop (x: Int, y: Int, n: Int): Int = n match {
-            case 0 => x + y
-            case i => loop(y, x + y, i - 1)
-        }
-        x match {
-         case 1 => 0
-         case 2 => 1
-         case _ => loop(0, 1, x - 3)
-        }
-    }
+import Data.List
 
-    def main(args: Array[String]) {
-         /** This will handle the input and output**/
-         println(fibonacci(readInt()))
+f :: Int -> Int -> Int
+f x n = length . filter (== x) $ subSeqTotals $ takeWhile (<= x) $ map (^ n) [1..]
+    where subSeqTotals [] = []
+          subSeqTotals (y:ys) = let s = subSeqTotals ys
+                                    fs = filter ((<= x) . (+ y)) s
+                                in y : foldr((:) . (+ y)) s fs
 
-    }
-}
+main = do
+    x <- readLn
+    n <- readLn
+    putStrLn $ show $ f x n
