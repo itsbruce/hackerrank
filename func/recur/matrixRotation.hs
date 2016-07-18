ce06286d73f649356f374c35ac854007485081ad
Bruce Richardson <it.is.bruce@gmail.com>
2016-07-18 18:23:01 +0100

    Matrix Rotation partly broken (only works for symmetrical matrices)

--- func/recur/matrixRotation.hs
+++ func/recur/matrixRotation.hs
@@ -1,5 +1,5 @@
 import Control.Monad.Reader
-import Data.Map.Strict as M
+import qualified Data.Map.Strict as M
 
 
 {- To rotate the matrix anti-clockwise, we move clockwise from
@@ -16,26 +16,32 @@ type Matrix = M.Map (Int, Int) String
 type App = Reader (Distance, Distance)
 
 
-width = 5
-depth = 4
+askWidth :: App Distance
+askWidth = fmap fst ask
+
+askDepth :: App Distance
+askDepth = fmap snd ask
 
 nextDirection Up = Across
 nextDirection x = succ x
 
-dimension Across = width
-dimension Back = width
-dimension _ = depth
+dimension Across = askWidth
+dimension Back = askWidth
+dimension _ = askDepth
 
 room size (x, y)
     | x < y = 0
     | otherwise =  max 0 $ size - 1 - x - y
 
-asIfAcross Across l = l
-asIfAcross Down (x, y) = (y, width - x - 1)
-asIfAcross Back (x, y) = (width - x - 1, depth - y - 1)
-asIfAcross Up (x, y) = (depth - y - 1, x)
+asIfAcross Across l = return l
+asIfAcross Down (x, y) = askWidth >>= ((\w -> return (y, w - x - 1)))
+asIfAcross Back (x, y) = ask >>= (\(w, d) -> return (w - x - 1, d - y - 1))
+asIfAcross Up (x, y) = askDepth >>= (\d -> return (d - y - 1, x))
 
-roomIn d l = room (dimension d) (asIfAcross d l)
+roomIn d l = do
+    size <- dimension d
+    l' <- asIfAcross d l
+    return $ room size l'
 
 
 travel Across n (x, y) = (x + n, y)
@@ -43,17 +49,34 @@ travel Back n (x, y) = (x - n, y)
 travel Down n (x, y) = (x, y + n)
 travel Up n (x, y) = (x, y - n)
 
-move :: (Direction, Distance, Location) -> (Direction, Distance, Location)
-move x@(_, 0, l) = x
-move x@(d, n, l)
-        | n < roomForX = (d, max 0 (n - roomForX), go n)
-        | otherwise = (nextDirection d, n - roomForX, go roomForX)
+move :: (Direction, Distance, Location) -> App (Direction, Distance, Location)
+move x@(_, 0, l) = return x
+move x@(d, n, l) = do
+        roomForX <- roomIn d l
+        if n < roomForX
+            then return (d, max 0 (n - roomForX), go n)
+            else return (nextDirection d, n - roomForX, go roomForX)
     where
-        roomForX = roomIn d l
         go x = travel d x l
 
-clockwise n l = until done move (Across, n, l)
-    where
-        done (_, 0, _) = True
-        done _ = False
+moveTillDone x@(_, 0, l) = return x
+moveTillDone x = moveTillDone =<< move x
 
+clockwise n l =  fmap (\(_, _, x) -> x) (moveTillDone (Across, n, l))
+
+rotateMatrix n m w d = M.mapWithKey getClockwise m
+    where getClockwise k = const (m M.! (runReader (clockwise n k) (w, d)))
+
+loadMatrix = M.fromList . concat . (zipWith addXY [0..])
+    where
+        addXY y = zip (zip [0..] (repeat y))
+
+readMatrix y x = replicateM y $ fmap ((take x) . words) getLine
+
+unloadMatrix n m = 
+    map (\i -> unwords (M.elems (M.filterWithKey (\(_, y) _ -> y == i) m))) [0..(n-1)] 
+
+main = do
+    [y, x, n] <- fmap ((take 3) . (map read) . words) getLine
+    xs <- fmap loadMatrix (readMatrix y x)
+    mapM putStrLn $ unloadMatrix y $ rotateMatrix n xs x y
