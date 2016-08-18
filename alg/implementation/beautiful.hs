970ccc5ab9e451eb9bad8ac2b39864988f751631
Bruce Richardson <it.is.bruce@gmail.com>
2016-08-18 20:01:49 +0100

    Most efficient implementation of Beautiful Triples (hs)

--- alg/implementation/beautiful.hs
+++ alg/implementation/beautiful.hs
@@ -1,17 +1,32 @@
-import Control.Applicative ((<**>))
+{-# LANGUAGE ViewPatterns #-}
+import qualified Data.Sequence as S
 
-bsubs :: Int -> [Int] -> [[Int]]
-bsubs d [] = [[]]
-bsubs d (x:xs) = bsubs d xs >>= prepend x
+data Triple a = Once {top :: a} | Twice {top :: a} | Thrice {top :: a}
+    deriving Show
+
+promote d (Once x) = (id, Twice (x + d))
+promote d (Twice x) = ((Thrice (x + d) S.<|), Twice (x + d))
+promote d _ = error "Attempt to overpromote"
+
+enqueue d x (S.viewl -> S.EmptyL) = S.singleton $ Once x
+enqueue d x q@(S.viewl -> t S.:< ts) =
+    let (prepend, suffix) = promote d t
+        need = top t + d
+    in  case compare need x of
+            LT -> enqueue d x ts
+            EQ -> prepend $ ts S.|> suffix
+            GT -> q S.|> Once x
+
+triples d xs = go S.empty xs
     where
-        prepend x [] = [[],[x]]
-        prepend x ys@(y:_)
-            | length ys < 3 && x + d == y = [ys, x : ys]
-            | otherwise = [ys]
+        go _ [] = []
+        go q (x:xs) = go' (enqueue d x q) xs
+        go' (S.viewl -> (Thrice n) S.:< q) = (n :) . go q
+        go' q = go q
 
 readN n = fmap (take n . map read . words) getLine
 
 main = do
     [n, d] <- readN 2
     xs <- readN n
-    print $ length $ filter ((== 3) . length) $ bsubs d xs
+    print $ length $ triples d xs
