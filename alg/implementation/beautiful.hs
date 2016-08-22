5b85cf91dce4176559027610da98531ca9129a45
Bruce Richardson <it.is.bruce@gmail.com>
2016-08-23 00:05:30 +0100

    Mostly piples version of Beautiful Triples

--- alg/implementation/beautiful.hs
+++ alg/implementation/beautiful.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE ViewPatterns #-}
+import Control.Monad (when)
 import qualified Data.Sequence as S
+import Pipes
+import qualified Pipes.Prelude as P
 
 enqueue d x (S.viewl -> S.EmptyL) = S.singleton (x, 1)
 enqueue d x q@(S.viewl -> s@(top, n) S.:< ss) =
@@ -9,18 +12,21 @@ enqueue d x q@(S.viewl -> s@(top, n) S.:< ss) =
             EQ -> ss S.|> (need, n + 1)
             GT -> q S.|> (x, 1)
 
-triples d xs = go S.empty xs
+triples d = go S.empty
     where
-        go _ [] = []
-        go q (x:xs) = go' (enqueue d x q) xs
-        go' q@(S.viewr -> _ S.:> (x, n)) = if n > 2
-            then (x :) . go q
-            else go q
-        go' q = go q
+        go q = do
+            x <- await
+            let q' = enqueue d x q
+            when (thrice x q') $ yield x
+            go q'
+        thrice x (S.viewr -> _ S.:> (_, n)) = n > 2
+        thrice _ _ = False
+
+countTriples d xs = head $ runEffect $ P.length $ each xs >-> triples d
 
 readN n = fmap (take n . map read . words) getLine
 
 main = do
     [n, d] <- readN 2
     xs <- readN n
-    print $ length $ triples d xs
+    print $ countTriples d xs
