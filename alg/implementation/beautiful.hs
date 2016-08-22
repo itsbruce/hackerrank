bfb3c526ab445cdc9bc48ab7b97f465c0f9361b3
Bruce Richardson <it.is.bruce@gmail.com>
2016-08-23 00:03:53 +0100

    Simpler code for Beautiful Triples

--- alg/implementation/beautiful.hs
+++ alg/implementation/beautiful.hs
@@ -1,27 +1,21 @@
 {-# LANGUAGE ViewPatterns #-}
 import qualified Data.Sequence as S
 
-data Triple a = Once {top :: a} | Twice {top :: a} | Thrice {top :: a}
-    deriving Show
-
-promote d (Once x) = (id, Twice (x + d))
-promote d (Twice x) = ((Thrice (x + d) S.<|), Twice (x + d))
-promote d _ = error "Attempt to overpromote"
-
-enqueue d x (S.viewl -> S.EmptyL) = S.singleton $ Once x
-enqueue d x q@(S.viewl -> t S.:< ts) =
-    let (prepend, suffix) = promote d t
-        need = top t + d
+enqueue d x (S.viewl -> S.EmptyL) = S.singleton (x, 1)
+enqueue d x q@(S.viewl -> s@(top, n) S.:< ss) =
+    let need = top + d
     in  case compare need x of
-            LT -> enqueue d x ts
-            EQ -> prepend $ ts S.|> suffix
-            GT -> q S.|> Once x
+            LT -> enqueue d x ss
+            EQ -> ss S.|> (need, n + 1)
+            GT -> q S.|> (x, 1)
 
 triples d xs = go S.empty xs
     where
         go _ [] = []
         go q (x:xs) = go' (enqueue d x q) xs
-        go' (S.viewl -> (Thrice n) S.:< q) = (n :) . go q
+        go' q@(S.viewr -> _ S.:> (x, n)) = if n > 2
+            then (x :) . go q
+            else go q
         go' q = go q
 
 readN n = fmap (take n . map read . words) getLine
