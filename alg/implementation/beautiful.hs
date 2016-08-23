e0c749489a4e22b064623eaa165c2a1e1dc63d2b
Bruce Richardson <it.is.bruce@gmail.com>
2016-08-23 23:02:37 +0100

    Final clean non-pipes Beautiful Triples solution

--- alg/implementation/beautiful.hs
+++ alg/implementation/beautiful.hs
@@ -1,28 +1,23 @@
 {-# LANGUAGE ViewPatterns #-}
-import Control.Monad (when)
 import qualified Data.Sequence as S
-import Pipes
-import qualified Pipes.Prelude as P
 
 enqueue d x (S.viewl -> S.EmptyL) = S.singleton (x, 1)
-enqueue d x q@(S.viewl -> s@(top, n) S.:< ss) =
+enqueue d x q@(S.viewl -> (top, n) S.:< ss) =
     let need = top + d
     in  case compare need x of
             LT -> enqueue d x ss
             EQ -> ss S.|> (need, n + 1)
             GT -> q S.|> (x, 1)
 
-triples d = go S.empty
+intervals d = go S.empty
     where
-        go q = do
-            x <- await
-            let q' = enqueue d x q
-            when (thrice x q') $ yield x
-            go q'
-        thrice x (S.viewr -> _ S.:> (_, n)) = n > 2
-        thrice _ _ = False
+        go _ [] = []
+        go q (x:xs) = go' (enqueue d x q) xs
+        go' q@(S.viewr -> _ S.:> x) = (x :) . go q
 
-countTriples d xs = head $ runEffect $ P.length $ each xs >-> triples d
+triples = filter ((> 2) . snd)
+
+countTriples d = length . triples . intervals d
 
 readN n = fmap (take n . map read . words) getLine
 
