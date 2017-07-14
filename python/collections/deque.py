798002cf618804cea54b5720f983aafe356971b8
Bruce Richardson <it.is.bruce@gmail.com>
2017-07-14 10:41:36 +0100

    Last message too soon

--- python/collections/deque.py
+++ python/collections/deque.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python2
 from collections import deque
 
-if __name__ == '__main__':
-    commands = {
-        'append' : (lambda d,x: d.append(x)),
-        'appendleft' : (lambda d,x: d.appendleft(x)),
-        'pop' : (lambda d: d.pop()),
-        'popleft' : (lambda d: d.popleft()),
-    }
+commands = {
+    'append' : (lambda d,x: d.append(x)),
+    'appendleft' : (lambda d,x: d.appendleft(x)),
+    'pop' : (lambda d: d.pop()),
+    'popleft' : (lambda d: d.popleft()),
+}
 
-    def runCommand(d, command, arg = None):
-        f = commands[command]
-        if arg == None:
-            f(d)
-        else:
-            f(d,arg)
-            
-    def parseInput(s):
-        xs = s.split(' ')
-        return xs[0], xs[1] if len(xs) > 1 else None
-    
+def runCommand(d, command, arg = None):
+    f = commands[command]
+    if arg == None:
+        f(d)
+    else:
+        f(d,arg)
+        
+def parseInput(s):
+    xs = s.split(' ')
+    return xs[0], xs[1] if len(xs) > 1 else None
+
+if __name__ == '__main__':
     dq = deque()
     n = int(raw_input())
     while n > 0:
