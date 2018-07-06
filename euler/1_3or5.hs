67f8a4574e1dbd9386524c221cbd1309cc48c0a8
Bruce Richardson <it.is.bruce@gmail.com>
2018-07-06 11:40:10 +0100

    Actual code for naive Euler 1

--- euler/1_3or5.hs
+++ euler/1_3or5.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+
 or35 = go [3,6..] [5,10..]
     where
         go xs@(x:xrest) ys@(y:yrest)
@@ -5,4 +7,14 @@ or35 = go [3,6..] [5,10..]
             | x == y = x : (go xrest yrest)
             | otherwise = y : (go xs yrest)
 
+sumLessThan:: Integer -> Integer
 sumLessThan n = sum $ takeWhile (< n) or35
+
+tests :: Int -> IO [Integer]
+tests n = replicateM n readLn
+
+main = do
+    t <- readLn
+    ts <- tests t
+    let results = map sumLessThan ts
+    mapM (putStrLn . show) results
